04ce1db31c360cfb545ba46966eafbf913d24575
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-04-08 15:47:02 +0200

    Initial import

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+FROM openjdk:8-jre
+
+ENV APACHEDS_ACCESS_CONTROL_ENABLED=1 \
+    APACHEDS_ADMIN_PASSWORD=secret \
+    APACHEDS_DOMAIN_NAME=example \
+    APACHEDS_DOMAIN_SUFFIX=com \
+    APACHEDS_INSTANCE_NAME=default \
+    APACHEDS_VERSION=2.0.0-M24-SNAPSHOT \
+    DUMBINIT_VERSION=1.2.0 \
+    DUMBINIT_SHA256SUM=81231da1cd074fdc81af62789fead8641ef3f24b6b07366a1c34e5b059faf363 \
+    DEBIAN_FRONTEND=noninteractive
+
+COPY KEYS /tmp/
+COPY apacheds-2.0.0-M24-SNAPSHOT-amd64.deb /tmp/
+
+RUN set -x \
+    && apt-get update -qq && apt-get install -qq -y --no-install-recommends ca-certificates gettext-base ldap-utils netcat wget && rm -rf /var/lib/apt/lists/* \
+    && wget -q -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v${DUMBINIT_VERSION}/dumb-init_${DUMBINIT_VERSION}_amd64 \
+    && echo "${DUMBINIT_SHA256SUM}  /usr/local/bin/dumb-init" > /tmp/SHA256SUM \
+    && sha256sum -c /tmp/SHA256SUM \
+    && rm /tmp/SHA256SUM \
+    && chmod +x /usr/local/bin/dumb-init \
+    #&& wget -q -O /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb http://ftp.halifax.rwth-aachen.de/apache/directory/apacheds/dist/${APACHEDS_VERSION}/apacheds-${APACHEDS_VERSION}-amd64.deb \
+    #&& wget -q -O /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb.asc https://archive.apache.org/dist/directory/apacheds/dist/${APACHEDS_VERSION}/apacheds-${APACHEDS_VERSION}-amd64.deb.asc \
+    && export GNUPGHOME="$(mktemp -d)" \
+    && gpg --import /tmp/KEYS \
+    #&& gpg --batch --verify /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb.asc /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb \
+    && dpkg -i /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb \
+    && ln -s /opt/apacheds-${APACHEDS_VERSION}/bin/apacheds /usr/local/bin/ \
+    && mv /var/lib/apacheds-${APACHEDS_VERSION} /var/lib/apacheds \
+    && sed -ie 's/^INSTANCES_DIRECTORY=.*/INSTANCES_DIRECTORY="\/var\/lib\/apacheds"/g' /opt/apacheds-${APACHEDS_VERSION}/bin/apacheds \
+    #&& rm -r "$GNUPGHOME" /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb /tmp/apacheds-${APACHEDS_VERSION}-amd64.deb* \
+    && apt-get purge -y --auto-remove wget
+
+COPY ldif/ /ldif/
+COPY templates/ /templates/
+COPY entrypoint.sh /usr/local/bin/
+
+EXPOSE 10389 10636 60088 60464 8080 8443
+VOLUME ["/var/lib/apacheds"]
+
+ENTRYPOINT ["/usr/local/bin/dumb-init", "--"]
+CMD ["/usr/local/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+/usr/local/bin/apacheds start ${APACHEDS_INSTANCE_NAME}
+while ! nc -z localhost 389; do
+  sleep 0.1
+done
+sleep 1
+
+if [ "${APACHEDS_ADMIN_PASSWORD}" != "secret" ] && [ ! -e "/var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.password-set" ]; then
+  echo "*** Setting admin password..."
+
+  envsubst < "/templates/admin_password.ldif" > "/tmp/admin_password.ldif"
+  ldapmodify -c -a -f /tmp/admin_password.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w secret
+  touch /var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.password-set
+fi
+
+if [ -n "${APACHEDS_ACCESS_CONTROL_ENABLED}" ] && [ ! -e "/var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.access" ]; then
+  echo "*** Enabling access control..."
+
+  ldapmodify -c -a -f /ldif/access.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w ${APACHEDS_ADMIN_PASSWORD}
+  touch /var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.access
+  /usr/local/bin/apacheds stop ${APACHEDS_INSTANCE_NAME}
+  /usr/local/bin/apacheds start ${APACHEDS_INSTANCE_NAME}
+  while ! nc -z localhost 10389; do
+    sleep 0.1
+  done
+  sleep 1
+fi
+
+if [ "${APACHEDS_DOMAIN_NAME}" != "example" ] || [ "${APACHEDS_DOMAIN_SUFFIX}" != "com" ]; then
+  if [ ! -e "/var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.domain-created" ]; then
+    echo "*** Creating partition ${APACHEDS_DOMAIN_NAME} for domain dc=${APACHEDS_DOMAIN_NAME},dc=${APACHEDS_DOMAIN_SUFFIX}..."
+
+    envsubst < "/templates/partition.ldif" > "/tmp/partition.ldif"
+    ldapmodify -c -a -f /tmp/partition.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w ${APACHEDS_ADMIN_PASSWORD}
+    ldapdelete "ads-partitionId=example,ou=partitions,ads-directoryServiceId=default,ou=config" -r -p 10389 -h localhost -D "uid=admin,ou=system" -w ${APACHEDS_ADMIN_PASSWORD}
+    ldapdelete "dc=example,dc=com" -p 10389 -h localhost -D "uid=admin,ou=system" -r -w ${APACHEDS_ADMIN_PASSWORD}
+    /usr/local/bin/apacheds stop ${APACHEDS_INSTANCE_NAME}
+    /usr/local/bin/apacheds start ${APACHEDS_INSTANCE_NAME}
+    while ! nc -z localhost 10389; do
+      sleep 0.1
+    done
+    sleep 1
+    envsubst < "/templates/top_domain.ldif" > "/tmp/top_domain.ldif"
+    ldapmodify -c -a -f /tmp/top_domain.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w ${APACHEDS_ADMIN_PASSWORD}
+    touch /var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.domain-created
+  fi
+fi
+
+if [ -n "${APACHEDS_ACCESS_CONTROL_ENABLED}" ] && [ ! -e "/var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.access_config" ]; then
+  echo "*** Enabling access control..."
+
+  envsubst < "/templates/access_config.ldif" > "/tmp/access_config.ldif"
+  ldapmodify -c -a -f /tmp/access_config.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w ${APACHEDS_ADMIN_PASSWORD}
+  touch /var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.access_config
+fi
+
+if [ -d /ldif.d/ ] && [ ! -e "/var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.ldif.d" ]; then
+  for f in /ldif.d/*.ldif; do
+    echo "*** Importing ${f}..."
+    ldapmodify -c -a -f ${f} -h localhost -p 10389 -D "uid=admin,ou=system" -w ${APACHEDS_ADMIN_PASSWORD}
+  done
+  touch /var/lib/apacheds/${APACHEDS_INSTANCE_NAME}/.ldif.d
+fi
+
+/usr/local/bin/apacheds stop ${APACHEDS_INSTANCE_NAME}
+exec /usr/local/bin/apacheds console ${APACHEDS_INSTANCE_NAME}
